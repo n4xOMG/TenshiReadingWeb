5f1ad58066863728991b5c6e32be80842bb8979f
Nguyễn Huỳnh Duy Tân <92073232+nixOMG@users.noreply.github.com>
2024-09-16 19:58:42 +0700

    update api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://52.220.66.16:5454";
+export const API_BASE_URL = "http://localhost:80";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
